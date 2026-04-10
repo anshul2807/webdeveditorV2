a556ec080e592d166848931fad4e971dc35fa506
Anshul singh <singhanshul2807@gmail.com>
2026-04-10 13:57:20 +0530

    updated baseURL

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://webdeveditor-backend-git-705615852872.asia-south2.run.app/api',
+  // baseURL: 'https://webdeveditor-backend-git-705615852872.asia-south2.run.app/api',
+  baseURL: 'https://webdeveditor-backend-203412458873.us-central1.run.app/api',
   headers: {
     'Content-Type': 'application/json',
   },
